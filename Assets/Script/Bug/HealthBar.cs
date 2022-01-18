8480713f50d6cf1731ce0c708edc8488eca558b8
Sisipun <sisipun@gmail.com>
2022-01-18 15:53:59 +0300

    Improve health bar with separators

--- Assets/Script/Bug/HealthBar.cs
+++ Assets/Script/Bug/HealthBar.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image fill;
+    [SerializeField] private GameObject separatorPrefub;
+    [SerializeField] private float separatorSize;
 
     private Slider slider;
+    private List<GameObject> separators;
 
     private int maxHealth;
     private int health;
@@ -16,12 +20,8 @@ public class HealthBar : MonoBehaviour
 
     void Awake()
     {
+        this.separators = new List<GameObject>();
         this.slider = GetComponent<Slider>();
-    }
-
-    void Update()
-    {
-        this.slider.value = health;
     }
 
     public void Init(int health, Color color)
@@ -31,6 +31,29 @@ public class HealthBar : MonoBehaviour
         this.slider.maxValue = maxHealth;
         this.slider.value = health;
         this.fill.color = color;
+
+        for (int i = 0; i < this.maxHealth - 1; i++)
+        {
+            if (i >= this.separators.Count)
+            {
+                this.separators.Add(Instantiate(separatorPrefub, this.transform));
+            }
+            this.separators[i].SetActive(true);
+        }
+        for (int i = this.maxHealth - 1; i < this.separators.Count; i++)
+        {
+            this.separators[i].SetActive(false);
+        }
+
+        RectTransform transform = this.GetComponent<RectTransform>();
+        float separatorDeltaX = transform.rect.width / maxHealth;
+        for (int i = 0; i < this.maxHealth - 1; i++)
+        {
+            RectTransform separatorTransform = this.separators[i].GetComponent<RectTransform>();
+            float separatorX = (i + 1) * separatorDeltaX;
+            separatorTransform.offsetMin = new Vector2(separatorX, separatorTransform.offsetMin.y);
+            separatorTransform.offsetMax = new Vector2((separatorX + separatorSize) - transform.rect.width, separatorTransform.offsetMax.y);
+        }
     }
 
     public void IncreaseHealth(int amount)
